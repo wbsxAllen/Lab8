d1b58e7c33f0aaf988c3195baf10ce1a57ee6f0d
wbsxAllen <wbsxallen@gmail.com>
2024-10-22 16:14:54 -0600

    update on unit test file

--- app/src/test/java/com/example/lab8/CustomListTest.java
+++ app/src/test/java/com/example/lab8/CustomListTest.java
@@ -8,9 +8,50 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import java.sql.Array;
 import java.util.ArrayList;
 
 public class CustomListTest {
+    /**
+     * this gets size of the list
+     * @return
+     */
+    public int getCount() {
+        return cities.size();
+    }
 
+    /**
+     * this adds a city object to the list
+     * for the first phase it will be
+     empty *@param city
+     */
+    public void addCity(City city) {
+
+    }
+
+    private CustomList list;
+    /**
+     * create a mcklist for my citylist
+     * @return
+     */
+
+    public CustomList MockCityList() {
+        list = new CustomList(null, new ArrayList<>());
+        return list;
+    }
+
+    /**
+     * get the size of the list
+     * increase the list by adding a new city
+     * check if our current size matches the initial size plus one
+     */
+
+    @Test
+    public void addCityTest() {
+        list = MockCityList();
+        int listSize = list.getCount();
+        list.addCity(new City("Estevan", "SK"));
+        assertEquals(list.getCount(), listSize + 1);
+    }
 
 }
